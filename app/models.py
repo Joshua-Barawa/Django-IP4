5bc045db6b5ecb4dbe536d1308cfaef6a237a219
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-03-20 15:22:20 +0300

    created business form

--- app/models.py
+++ app/models.py
@@ -4,10 +4,10 @@ import datetime
 
 
 class Neighborhood(models.Model):
-    name = models.CharField()
-    location = models.CharField()
+    name = models.CharField(max_length=100, blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
     occupants_count = models.IntegerField(default=0)
-    admin = models.ForeignKey()
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.name)
@@ -16,17 +16,17 @@ class Neighborhood(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, blank=True, null=True, on_delete=models.CASCADE)
     profile_pic = models.ImageField(default='media/default2.png', upload_to='media/', null=True, blank=True)
-    neighborhood = models.ForeignKey(Neighborhood, null=True, blank=True)
+    neighborhood = models.ForeignKey(Neighborhood, null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.user.username)
 
 
 class Business(models.Model):
-    name = models.CharField()
-    user = models.ForeignKey(User)
-    neighborhood = models.ForeignKey(Neighborhood)
-    business_email = models.EmailField()
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    neighborhood = models.ForeignKey(Neighborhood, null=True, blank=True, on_delete=models.CASCADE)
+    business_email = models.CharField(max_length=100)
 
     def __str__(self):
         return str(self.name)
