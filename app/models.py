1df4beeb21c345833d37a559ce1c035ba65bc1c4
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-03-20 15:03:41 +0300

    created profie picture

--- app/models.py
+++ app/models.py
@@ -5,8 +5,8 @@ import datetime
 
 class Profile(models.Model):
     user = models.OneToOneField(User, blank=True, null=True, on_delete=models.CASCADE)
-    profile_pic = models.ImageField(null=True)
-    caption = models.TextField(max_length=200)
+    profile_pic = models.ImageField(default='media/default2.png', upload_to='media/', null=True, blank=True)
+    caption = models.TextField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return str(self.user.username)
--- app/views.py
+++ app/views.py
@@ -6,6 +6,11 @@ from .forms import *
 from .models import *
 
 
+@login_required(login_url='login-user/')
+def index(request):
+    return render(request, 'html/index.html', {})
+
+
 def register_user(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
@@ -25,6 +30,23 @@ def register_user(request):
     else:
         form = RegisterForm()
     return render(request, 'html/register.html', {"form": form})
+
+
+@login_required(login_url='login-user/')
+def my_profile(request):
+    user = request.user
+    profile = Profile.objects.get(user=user)
+    if request.method == "POST":
+        username = request.POST.get('username')
+        fullname = request.POST.get('fullname')
+        email = request.POST.get('email')
+        profile.profile_pic = request.FILES['profile_pic']
+        profile.caption = request.POST.get("bio")
+
+        user = User.objects.filter(username=user.username).update(username=username, first_name=fullname, email=email)
+        profile.save()
+    return render(request, 'html/profile.html', {"profile": profile})
+
 
 def login_user(request):
     if request.method == "POST":
