1014836b948eb4a00d7d164be27748a4c71ef30b
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-03-20 15:13:36 +0300

    created business model

--- app/models.py
+++ app/models.py
@@ -3,17 +3,30 @@ from django.contrib.auth.models import User
 import datetime
 
 
+class Neighborhood(models.Model):
+    name = models.CharField()
+    location = models.CharField()
+    occupants_count = models.IntegerField(default=0)
+    admin = models.ForeignKey()
+
+    def __str__(self):
+        return str(self.name)
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, blank=True, null=True, on_delete=models.CASCADE)
     profile_pic = models.ImageField(default='media/default2.png', upload_to='media/', null=True, blank=True)
-    caption = models.TextField(max_length=200, null=True, blank=True)
+    neighborhood = models.ForeignKey(Neighborhood, null=True, blank=True)
 
     def __str__(self):
         return str(self.user.username)
 
 
-class Neighbourhood(models.Model):
+class Business(models.Model):
     name = models.CharField()
-    location = models.CharField()
-    occupants_count = models.IntegerField(default=0)
-    admin = models.ForeignKey()
+    user = models.ForeignKey(User)
+    neighborhood = models.ForeignKey(Neighborhood)
+    business_email = models.EmailField()
+
+    def __str__(self):
+        return str(self.name)
