359547a5a792076d3ea86e206fb4b9fb3dcef30b
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-03-20 15:49:06 +0300

    created post form

--- app/forms.py
+++ app/forms.py
@@ -38,7 +38,18 @@ class BusinessForm(forms.ModelForm):
         fields = ['name', "neighborhood", "email"]
 
     def __init__(self, *args, **kwargs):
-        super(RegisterForm, self).__init__(*args, **kwargs)
+        super(BusinessForm, self).__init__(*args, **kwargs)
         self.fields['name'].widget.attrs['class'] = 'form-control'
         self.fields['neighborhood'].widget.attrs['class'] = 'form-control'
         self.fields['email'].widget.attrs['class'] = 'form-control'
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', "description"]
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs['class'] = 'form-control'
+        self.fields['description'].widget.attrs['class'] = 'form-control'
